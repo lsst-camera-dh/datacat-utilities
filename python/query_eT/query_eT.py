a1537aed3f028cd2850565888785fd7c869ba4fb
richardxdubois <richard.x.dubois@gmail.com>
2017-01-16 18:47:34 -0800

    add finding DC virtual paths for datasets

--- python/query_eT/query_eT.py
+++ python/query_eT/query_eT.py
@@ -4,23 +4,27 @@ import numpy as np
 from scipy.stats import describe
 import collections
 import operator
+import os
 
 class query_eT():
 
-    def __init__(self, schemaName=None, valueName=None, ccdType=None,dataType=None,resultType='FloatResultHarnessed',dbConnectFile='db_connect.txt'):
+    def __init__(self, schemaName=None, valueName=None, ccdType=None,dataType=None,resultType='FloatResultHarnessed',resultName='value', device=None, dbConnectFile='db_connect.txt'):
         self.schemaName = schemaName
         self.valueName = valueName
         self.ccdType = ccdType
         self.dataType = dataType
         self.resultType = resultType
+        self.device = device
         self.dbConnectFile = dbConnectFile
 
-    def setParams(self, schemaName=None, valueName=None, ccdType=None,dataType=None,resultType=None, dbConnectFile=None):
+    def setParams(self, schemaName=None, valueName=None, ccdType=None,dataType=None,resultType=None, resultName=None, dbConnectFile=None):
         if schemaName is not None: self.schemaName = schemaName
         if valueName is not None:self.valueName = valueName
         if ccdType is not None:self.ccdType = ccdType
         if dataType is not None:self.dataType = dataType
         if resultType is not None:self.resultType = resultType
+        if resultName is not None:self.resultName = resultName
+        if devicee is not None:self.device = device
         if dbConnectFile is not None: self.dbConnectFile = dbConnectFile
 
     def connectDB(self):
@@ -63,9 +67,11 @@ class query_eT():
 
     def queryResultsDB(self,engine):
 
+        dev_query = ''
+        if self.device is not None: dev_query = ' and hw.lsstId = ' + self.device
 
 
-        sqlVendor = "select hw.lsstId, res.activityId, act.rootActivityId, res.value, res.schemaInstance from " + self.resultType + " res join Activity act on res.activityId=act.id JOIN Hardware hw ON act.hardwareId=hw.id join Process pr on act.processId=pr.id where lower(res.schemaName) ='" + self.schemaName + "' and res.name= '" + self.valueName + "' order by res.activityId asc"
+        sqlVendor = "select hw.lsstId, res.activityId, act.rootActivityId, res." + self.resultName + ", res.schemaInstance from " + self.resultType + " res join Activity act on res.activityId=act.id JOIN Hardware hw ON act.hardwareId=hw.id join Process pr on act.processId=pr.id where lower(res.schemaName) ='" + self.schemaName + "' and res.name= '" + self.valueName + dev_query + "' order by res.activityId asc"
 
         sql = sqlVendor
 
@@ -110,14 +116,88 @@ class query_eT():
                 if parent[row[0]] == row[2]:
                     if row[0] not in ccd.keys():
                         ccd[row[0]] = []
-                    ccd[row[0]].append([row[3],row[4]])
+                    value = row[3]
+                    if self.resultType == 'FilepathResultHarnessed': value = os.path.split(value)
+                    ccd[row[0]].append([value,row[4]])
 
-# sort the lists per key by the instance numbee as they are not returned in order
+# sort the lists per key by the instance number as they are not returned in order
 
         for key in ccd:
             ccd[key] = sorted(ccd[key],key=operator.itemgetter(1))
 
         return ccd
+
+    
+
+    def queryCatalogVirtualPaths(self,engine):
+
+        self.resultType = 'FilepathResultHarnessed'
+        self.resultName = 'virtualPath'
+
+        dev_query = ''
+        if self.device is not None: dev_query = "' and hw.lsstId = '" + self.device
+
+        
+        sqlVendor = "select hw.lsstId, res.activityId, act.rootActivityId, res." + self.resultName + ", res.schemaInstance from " + self.resultType + " res join Activity act on res.activityId=act.id JOIN StringResultHarnessed fl  on res.activityId=fl.activityId JOIN Hardware hw ON act.hardwareId=hw.id join Process pr on act.processId=pr.id where lower(fl.schemaName) ='" + self.schemaName + dev_query + "' order by res.activityId asc"
+
+        sql = sqlVendor
+
+        print sql
+
+
+        result = engine.execute(sql)
+
+        id = []
+        ccd = collections.OrderedDict()
+
+        parent = {}
+        query = []
+
+        # grab the query data, find the max parent id for each sensor (ie most recent run) and copy the result cursor into the query list
+
+        for row in result:
+            query.append([row['lsstId'], row['activityId'], row['rootActivityId'], row['virtualPath'], row['schemaInstance']])
+
+        print 'len(query) = ', len(query)
+
+        # get the parent traveler's id and name
+
+        sqlParent = "select hw.id, hw.lsstId, act.id parId, pr.name from Activity act join Hardware hw on act.hardwareId=hw.id join Process pr on act.processId=pr.id join ActivityStatusHistory statusHist on act.id=statusHist.activityId where  pr.name='" + self.dataType + "'"
+
+        print sqlParent
+
+        resultParent = engine.execute(sqlParent)
+
+        # only take data from the most recent run of the traveler
+
+        parentAct = {}
+
+        for row in resultParent:
+            parentId = parent.setdefault(row['lsstId'])
+            parent[row['lsstId']] = row['parId']
+
+        resultParent.close()
+
+        for row in query:
+            if self.ccdType in row[0] and row[0] in parent:
+                if parent[row[0]] == row[2]:
+                    if row[0] not in ccd.keys():
+                        ccd[row[0]] = []
+                    value = row[3]
+                    if self.resultType == 'FilepathResultHarnessed': value, junk = os.path.split(value)
+                    value += '/'
+                    ccd[row[0]].append([value,row[4]])
+
+# sort the lists per key by the instance number as they are not returned in order
+
+        for key in ccd:
+            ccd[key] = sorted(ccd[key],key=operator.itemgetter(1))
+
+# ccd is a dict, with CCD as key, and each key has a list of pairs - value and instance number (eg amp #)
+
+        return ccd
+
+
 
 if __name__ == "__main__":
     
