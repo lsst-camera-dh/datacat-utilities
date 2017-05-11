01dffd88e96ffd8da87b05e18e01f3eadf888fb1
Heather Kelly <heather@slac.stanford.edu>
2017-05-11 13:27:12 -0700

    use a script to run the travis commands and turn off email notification

---
+++ runlint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+flake8 --show-source --statistics --max-line-length=90 python
+FLAKERC=$?
+
+pylint `find . -name \*.py -print`
+PYLINTRC=$?
+# Fail Travis build if Pylint returns fatal (1) | error (2)
+if [ $(($PYLINTRC & 3)) -ne 0 ]; then
+    echo "Pylint failed"
+    exit 1
+else
+    echo "Pylint passed"
+    exit 0
+fi
