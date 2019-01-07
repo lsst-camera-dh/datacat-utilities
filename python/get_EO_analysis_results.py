163ad47ccf4244f95e7e2acdb721e1298b58c94f
Richard Dubois <dubois@stanford.edu>
2019-01-07 11:19:34 -0800

    more fixes for full fp files

--- python/get_EO_analysis_results.py
+++ python/get_EO_analysis_results.py
@@ -1,6 +1,9 @@
 """Helper tools to fetch Camera EO Test analysis results."""
 
 from eTraveler.clientAPI.connection import Connection
+from exploreFocalPlane import exploreFocalPlane
+from exploreRaft import exploreRaft
+
 import numpy as np
 import collections
 import argparse
@@ -45,6 +48,7 @@ class get_EO_analysis_results():
 
         # define hardware types and traveler names, respectively
 
+        self.site_type['I&T-BOT'] = ['LCA-10134_Cryostat-0001', 'INT-SR-EOT-01']  # supply hw instance name
         self.site_type['I&T-Raft'] = ['LCA-11021_RTM', 'INT-SR-EOT-01']
         self.site_type['BNL-Raft'] = ['LCA-11021_RTM', 'SR-RTM-EOT-03']
         self.site_type['BNL-ITL-CCD'] = ['ITL-CCD', 'SR-EOT-1']
@@ -53,11 +57,58 @@ class get_EO_analysis_results():
         self.site_type['vendor-ITL'] = ['ITL-CCD', 'SR-EOT-02']
 
         self.type_dict = {}
+        self.type_dict_BOT = {}
         self.type_dict_raft = {}
         self.type_dict_ccd = {}
 
         # define step and schema names, respectively
 
+        self.type_dict_BOT['gain'] = ['fe55_BOT_analysis', 'fe55_BOT_analysis']
+        self.type_dict_BOT['gain_error'] = ['fe55_BOT_analysis', 'fe55_BOT_analysis']
+        self.type_dict_BOT['psf_sigma'] = ['fe55_BOT_analysis', 'fe55_BOT_analysis']
+        self.type_dict_BOT['cti_low_serial'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_high_serial'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_low_parallel'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_high_parallel'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_low_serial_error'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_high_serial_error'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_low_parallel_error'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['cti_high_parallel_error'] = ['cte_BOT', 'cte_BOT']
+        self.type_dict_BOT['read_noise'] = ['read_noise_BOT', 'read_noise_BOT']
+        self.type_dict_BOT['system_noise'] = ['read_noise_BOT', 'read_noise_BOT']
+        self.type_dict_BOT['total_noise'] = ['read_noise_BOT', 'read_noise_BOT']
+        self.type_dict_BOT['bright_pixels'] = ['bright_defects_BOT', 'bright_defects_BOT']
+        self.type_dict_BOT['bright_columns'] = ['bright_defects_BOT', 'bright_defects_BOT']
+        self.type_dict_BOT['dark_pixels'] = ['dark_defects_BOT', 'dark_defects_BOT']
+        self.type_dict_BOT['dark_columns'] = ['dark_defects_BOT', 'dark_defects_BOT']
+        self.type_dict_BOT['num_traps'] = ['traps_BOT', 'traps_BOT']
+        self.type_dict_BOT['dark_current_95CL'] = ['dark_current_BOT', 'dark_current_BOT']
+        self.type_dict_BOT['ptc_gain'] = ['ptc_BOT', 'ptc_BOT']
+        self.type_dict_BOT['ptc_gain_error'] = ['ptc_BOT', 'ptc_BOT']
+
+        self.type_dict_BOT['QE'] = ['qe_BOT_analysis', 'qe_BOT_analysis']
+        self.type_dict_BOT['full_well'] = ['flat_pairs_BOT_analysis', 'flat_pairs_BOT']
+        self.type_dict_BOT['max_frac_dev'] = ['flat_pairs_BOT_analysis', 'flat_pairs_BOT']
+
+        self.type_dict['BOT'] = self.type_dict_BOT
+
+        self.BOT_schema_meas = {}
+        self.BOT_schema_meas['fe55_BOT_analysis'] = ['gain', 'gain_error', 'psf_sigma']
+        self.BOT_schema_meas['read_noise_BOT'] = ['read_noise', 'system_noise', 'total_noise']
+        self.BOT_schema_meas['bright_defects_BOT'] = ['bright_pixels', 'bright_columns']
+        self.BOT_schema_meas['dark_defects_BOT'] = ['dark_pixels', 'dark_columns']
+        self.BOT_schema_meas['dark_current_BOT'] = ['dark_current_95CL']
+        self.BOT_schema_meas['traps_BOT'] = ['num_traps']
+        self.BOT_schema_meas['cte_BOT'] = ['cti_high_serial', 'cti_high_serial_error',
+                                           'cti_high_parallel', 'cti_high_parallel_error', 'cti_low_serial',
+                                           'cti_low_serial_error',
+                                           'cti_low_parallel', 'cti_low_parallel_error']
+        self.BOT_schema_meas['flat_pairs_BOT'] = ['full_well', 'max_frac_dev']
+        self.BOT_schema_meas['ptc_BOT'] = ['ptc_gain', 'ptc_gain_error']
+        self.BOT_schema_meas['qe_BOT_analysis'] = ['QE']
+        self.BOT_schema_meas['tearing_detection_BOT'] = ['detections']
+
+        #        self.type_dict_raft['pixel_mean'] = ['prnu_raft', 'prnu']
         self.type_dict_raft['gain'] = ['fe55_raft_analysis', 'fe55_raft_analysis']
         self.type_dict_raft['gain_error'] = ['fe55_raft_analysis', 'fe55_raft_analysis']
         self.type_dict_raft['psf_sigma'] = ['fe55_raft_analysis', 'fe55_raft_analysis']
@@ -80,7 +131,7 @@ class get_EO_analysis_results():
         self.type_dict_raft['dark_current_95CL'] = ['dark_current_raft', 'dark_current_raft']
         self.type_dict_raft['ptc_gain'] = ['ptc_raft', 'ptc_raft']
         self.type_dict_raft['ptc_gain_error'] = ['ptc_raft', 'ptc_raft']
-        #        self.type_dict_raft['pixel_mean'] = ['prnu_raft', 'prnu']
+
         self.type_dict_raft['QE'] = ['qe_raft_analysis', 'qe_raft_analysis']
         self.type_dict_raft['full_well'] = ['flat_pairs_raft_analysis', 'flat_pairs_raft']
         self.type_dict_raft['max_frac_dev'] = ['flat_pairs_raft_analysis', 'flat_pairs_raft']
@@ -123,6 +174,8 @@ class get_EO_analysis_results():
         else:
             pS = False
         self.connect = Connection(operator='richard', db=db, exp='LSST-CAMERA', prodServer=pS)
+        self.eFP = exploreFocalPlane(db=db, prodServer=server)
+        self.eR = exploreRaft(db=db, prodServer=server)
 
     def get_tests(self, site_type=None, test_type=None, run=None):
         """
@@ -139,7 +192,11 @@ class get_EO_analysis_results():
             dev_list: list of hardware devices associated with the query
             data: object containing data from the query to getResultsXXX
         """
-        if 'Raft' not in site_type:
+        if 'Raft' in site_type:
+            self.camera_type = 'raft'
+        elif "BOT" in site_type:
+            self.camera_type = 'BOT'
+        else:
             self.camera_type = 'ccd'
 
         dev_list = []
@@ -158,9 +215,16 @@ class get_EO_analysis_results():
             if test_type is None:
                 data = self.connect.getRunResults(run=run)
             else:
-                data = self.connect.getRunResults(run=run,
-                                                  stepName=self.type_dict[self.camera_type][test_type][0])
-            dev_list = data['experimentSN']
+                stepName = self.type_dict[self.camera_type][test_type][0]
+                if self.camera_type == "BOT":
+                    stepName = "BOT_EO_analysis"
+                data = self.connect.getRunResults(run=run, stepName=stepName)
+            if self.camera_type == "BOT":
+                rl = self.eFP.focalPlaneContents(parentName=self.site_type["I&T-BOT"][0], run=run)
+                for r in rl:
+                    dev_list.append(r[0])
+            else:
+                dev_list = data['experimentSN']
 
         # this step gives us dark columns and dark pixels
 
@@ -177,28 +241,57 @@ class get_EO_analysis_results():
             device: specific hardware identifier desired
 
         Output:
-            ccd_dict: dictionary of lists of values (eg gains) keyed on CCD name
+            ccd_dict: single raft or CCD - dictionary of lists of values (eg gains) keyed on CCD name
+                        full FP: dict (by test) of dict by raft slot by dict of ccd slot containing lists
+                        of results. Note the use of slots (Rxx, Sxx) not raft or CCD names.
         """
         ccd_dict = collections.OrderedDict()
+        test_dict = collections.OrderedDict()
 
         ccdName = None
         if self.camera_type == 'ccd':
             ccdName = device
 
-        try:
-            data[device]
-            step = data[device]['steps'][self.type_dict[self.camera_type][test_type][0]]
-        except KeyError:
-            step = data['steps'][self.type_dict[self.camera_type][test_type][0]]
+        if self.camera_type is not "BOT":
 
-        for amp in step[self.type_dict[self.camera_type][test_type][1]][1:]:
-            if self.camera_type == 'raft':
-                ccdName = amp['sensor_id']
-            c = ccd_dict.setdefault(ccdName, [])
-            ampResult = amp[test_type]
-            c.append(ampResult)
+            try:
+                data[device]
+                step = data[device]['steps'][self.type_dict[self.camera_type][test_type][0]]
+            except KeyError:
+                step = data['steps'][self.type_dict[self.camera_type][test_type][0]]
 
-        return ccd_dict
+            for amp in step[self.type_dict[self.camera_type][test_type][1]][1:]:
+                if self.camera_type == 'raft':
+                    ccdName = amp['sensor_id']
+                c = ccd_dict.setdefault(ccdName, [])
+                ampResult = amp[test_type]
+                c.append(ampResult)
+            return ccd_dict
+
+        else:
+            t_dict = data['steps']["BOT_EO_analysis"]
+            # find schema for test type
+            test_name_type = ""
+            for schemas in self.BOT_schema_meas:
+                if test_type in self.BOT_schema_meas[schemas]:
+                    test_name_type = schemas
+
+            for a in t_dict[test_name_type][1:]:
+                ccd_slot = a["slot"]
+                raft_slot = a["raft"]
+
+                if test_type == "QE":
+                    band = a["band"]
+                    qe_band = test_type + "-" + str(band)
+                    t = test_dict.setdefault(qe_band, {})
+                else:
+                    t = test_dict.setdefault(test_type, {})
+                r = t.setdefault(raft_slot, {})
+                c = r.setdefault(ccd_slot, [])
+                meas = a[test_type]
+                c.append(meas)
+
+            return test_dict
 
     def get_all_results(self, data=None, device=None):
         """
@@ -210,33 +303,63 @@ class get_EO_analysis_results():
             device: specific hardware identifier desired
 
         Output:
-            test_dict: dictionary (by test, eg gain) of dictionary (by CCD) of lists of values (eg gains)
+            test_dict: single raft: dictionary (by test, eg gain) of dictionary (by CCD) of lists of values
+                        full FP: dict (by test) of dict by raft slot by dict of ccd slot containing lists
+                        of results. Note the use of slots (Rxx, Sxx) not raft or CCD names.
+            (eg gains)
         """
         test_dict = collections.OrderedDict()
+        # get the list of supported tests from member dict
+        test_list = self.type_dict[self.camera_type]
 
         ccdName = None
         if self.camera_type == 'ccd':
             ccdName = device
 
-        # get the list of supported tests from member dict
-        test_list = self.type_dict[self.camera_type]
+        if self.camera_type is not "BOT":
 
-        for tests in test_list:
-            steps = test_list[tests][0]
-            test_name_type = test_list[tests][1]
+            for tests in test_list:
+                steps = test_list[tests][0]
+                test_name_type = test_list[tests][1]
 
-            # set up empty dict, keyed off test name
-            t = test_dict.setdefault(tests, {})
+                # set up empty dict, keyed off test name
+                t = test_dict.setdefault(tests, {})
 
-            # test_name_type is the test's EO schema name
-            results_list = data['steps'][steps][test_name_type]
-            for amp in results_list[1:]:
-                if self.camera_type == 'raft':
-                    ccdName = amp['sensor_id']
-                # set up dict by CCD with list of test quantities
-                c = t.setdefault(ccdName, [])
-                ampResult = amp[tests]
-                c.append(ampResult)
+                # test_name_type is the test's EO schema name
+                results_list = data['steps'][steps][test_name_type]
+                for amp in results_list[1:]:
+                    if self.camera_type == 'raft':
+                        ccdName = amp['sensor_id']
+                    # set up dict by CCD with list of test quantities
+                    c = t.setdefault(ccdName, [])
+                    ampResult = amp[tests]
+                    c.append(ampResult)
+
+        else:
+            t_dict = data['steps']["BOT_EO_analysis"]
+            for test_name_type in t_dict:
+                if test_name_type == "job_info" or test_name_type == 'tearing_detection_BOT' or \
+                        test_name_type == "package_versions":
+                    continue
+
+                for a in t_dict[test_name_type][1:]:
+                    try:
+                        raft_slot = a["raft"]
+                        ccd_slot = a["slot"]
+                    except KeyError:
+                        print(a)
+
+                    for res in self.BOT_schema_meas[test_name_type]:
+                        if res == "QE":
+                            band = a["band"]
+                            qe_band = res + "-" + str(band)
+                            t = test_dict.setdefault(qe_band, {})
+                        else:
+                            t = test_dict.setdefault(res, {})
+                        r = t.setdefault(raft_slot, {})
+                        c = r.setdefault(ccd_slot, [])
+                        meas = a[res]
+                        c.append(meas)
 
         return test_dict
 
@@ -259,10 +382,11 @@ if __name__ == "__main__":
 
     g = get_EO_analysis_results(db=args.db, server=args.eTserver)
 
-    start = time.time()
-    raft_list, data = g.get_tests(site_type=args.site_type, test_type=args.test_type, run=args.run)
-    res = g.get_results(test_type=args.test_type, data=data, device=raft_list)
-    after_sngl = time.time() - start
+    if True:
+        start = time.time()
+        raft_list, data = g.get_tests(site_type=args.site_type, test_type=args.test_type, run=args.run)
+        res = g.get_results(test_type=args.test_type, data=data, device=raft_list)
+        after_sngl = time.time() - start
 
     raft_list_all, data_all = g.get_tests(site_type=args.site_type, run=args.run)
     res_all = g.get_all_results(data=data_all, device=raft_list_all)
